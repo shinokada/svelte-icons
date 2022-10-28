6e4e88950abb2ba7a84f103ecf8c9c32ca8a79f0
Shinichi Okada <okada.shin@gmail.com>
2022-10-29 06:48:02 +0900

    migrate svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { EcommerceCard, Badge } from 'flowbite-svelte';
+	import { Card, Badge } from 'flowbite-svelte';
 
 	let imgAnt = {
 		src: '/images/resized/ant.webp',
@@ -101,11 +101,11 @@
 
 <h2>Support all CSS frameworks</h2>
 
-<div class="flex flex-wrap justify-center">
-	<EcommerceCard
+<div class="flex flex-wrap justify-center gap-4">
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgAnt}
+		img={imgAnt.src}
 		title="Ant Design: Svelte-Ant-Design-Icons"
 		href="https://www.npmjs.com/package/svelte-ant-design-icons"
 	>
@@ -129,12 +129,12 @@
 			<a href="https://svelte.dev/repl/375c734a6ec74f44adfe4aa76b117ab9?version=3.50.1">REPL 1,</a>
 			<a href="https://svelte.dev/repl/38a6fcd635c14255b7a30c1a7d4b05c2?version=3.50.1">REPL 2</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgAwesome}
+		img={imgAwesome.src}
 		title="Font Awesome: Svelte-Awesome-Icons"
 		href="https://www.npmjs.com/package/svelte-awesome-icons"
 	>
@@ -157,12 +157,12 @@
 			><br />
 			<a href="https://svelte.dev/repl/c0045886b264408fba13f1de70c42932?version=3.48.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgBootstrap}
+		img={imgBootstrap.src}
 		title="Bootstrap: Svelte-Bootstrap-Svg-Icons"
 		href="https://www.npmjs.com/package/svelte-bootstrap-svg-icons"
 	>
@@ -183,12 +183,12 @@
 			><br />
 			<a href="https://svelte.dev/repl/1613cc34aef7417783284b74e250c8e8?version=3.48.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgCircleFlags}
+		img={imgCircleFlags.src}
 		title="Circle Flags: Svelte-Circle-Flags"
 		href="https://www.npmjs.com/package/svelte-circle-flags"
 	>
@@ -210,12 +210,12 @@
 			><br />
 			<a href="https://svelte.dev/repl/382095078be04da7a5008b7f5e41d5c8?version=3.48.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgCrypto}
+		img={imgCrypto.src}
 		title="Cryptocurrency: Svelte-Cryptocurrency-Icons"
 		href="https://www.npmjs.com/package/svelte-cryptocurrency-icons"
 	>
@@ -236,12 +236,12 @@
 				>Icon names</a
 			>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgFeather}
+		img={imgFeather.src}
 		title="Feather Icons: Svelte-Feathers"
 		href="https://www.npmjs.com/package/svelte-feathers"
 	>
@@ -259,12 +259,12 @@
 			280+ SVG icons from <a href="https://feathericons.com/">Feather-icons</a>.<br />
 			<a href="https://github.com/shinokada/svelte-feathers/blob/main/icon-list.md">Icon names</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgFlagIcons}
+		img={imgFlagIcons.src}
 		title="Flag-Icons: Svelte-Flag-Icons"
 		href="https://www.npmjs.com/package/svelte-flag-icons"
 	>
@@ -284,12 +284,12 @@
 			><br />
 			<a href="https://svelte.dev/repl/70a4c2d5242f4cfa8dba85529184d7a3?version=3.48.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgFlags}
+		img={imgFlags.src}
 		title="Country Flags: Svelte-Flags"
 		href="https://www.npmjs.com/package/svelte-flags"
 	>
@@ -300,12 +300,12 @@
 			>.<br />
 			<a href="https://github.com/shinokada/svelte-flags/blob/main/flags.md">Icon names</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgFile}
+		img={imgFile.src}
 		title="File Icons: Svelte-File-Icons"
 		href="https://www.npmjs.com/package/svelte-file-icons"
 	>
@@ -326,12 +326,12 @@
 			<br />
 			<a href="https://svelte.dev/repl/985ea64e08a942b493afa91538fef9b1?version=3.48.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgGoogle}
+		img={imgGoogle.src}
 		title="Google Materialdesign: Svelte-Google-Materialdesign-Icons"
 		href="https://www.npmjs.com/package/svelte-google-materialdesign-icons"
 	>
@@ -355,12 +355,12 @@
 				>Icon names</a
 			>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgHero}
+		img={imgHero.src}
 		title="Heroicons: Svelte-Heros"
 		href="https://www.npmjs.com/package/svelte-heros"
 	>
@@ -373,12 +373,12 @@
 			/>
 			<a href="https://svelte.dev/repl/48606ee88712441caebd7d09aa62602c?version=3.48.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgHero2}
+		img={imgHero2.src}
 		title="Heroicons v2: Svelte-Heros-v2"
 		href="https://www.npmjs.com/package/svelte-heros-v2"
 	>
@@ -400,12 +400,12 @@
 			<a href="https://svelte.dev/repl/1948d7b5157f44709cff380d9783342c?version=3.49.0">REPL 1</a>
 			<a href="https://svelte.dev/repl/e26c4ea516a0493cb64797879b1fe46d?version=3.49.0">REPL 2</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgIon}
+		img={imgIon.src}
 		title="Ionicons: Svelte-Ionicons"
 		href="https://www.npmjs.com/package/svelte-ionicons"
 	>
@@ -424,12 +424,12 @@
 			1330+ SVG icons from <a href="https://ionic.io/ionicons">Ionicicons</a>.<br />
 			<a href="https://github.com/shinokada/svelte-ionicons/blob/main/icon-list.md">Icon names</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgLucide}
+		img={imgLucide.src}
 		title="Lucide: Svelte-Lucide"
 		href="https://www.npmjs.com/package/svelte-lucide"
 	>
@@ -444,12 +444,12 @@
 			590+ SVG icons from <a href="https://github.com/lucide-icons/lucide">Lucide icons</a>.<br />
 			<a href="https://github.com/shinokada/svelte-lucide/blob/main/icon-list.md">Icon names</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgMaterial}
+		img={imgMaterial.src}
 		title="Materialdesign: Svelte-Materialdesign-Icons"
 		href="https://www.npmjs.com/package/svelte-materialdesign-icons"
 	>
@@ -471,12 +471,12 @@
 				>Icon names</a
 			>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgOct}
+		img={imgOct.src}
 		title="Octicons: Svelte-Oct"
 		href="https://www.npmjs.com/package/svelte-oct"
 	>
@@ -488,12 +488,12 @@
 			<a href="https://github.com/shinokada/svelte-oct/blob/main/icon-list.md">Icon names</a><br />
 			<a href="https://svelte.dev/repl/fccdaf257870448bbb6b924fda6c3a5e?version=3.49.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgRadix}
+		img={imgRadix.src}
 		title="Radix icons: Svelte-Radix"
 		href="https://www.npmjs.com/package/svelte-radix"
 	>
@@ -506,12 +506,12 @@
 			/>
 			<a href="https://svelte.dev/repl/fb0def38847240f3a51460a56bf53225?version=3.49.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgRemix}
+		img={imgRemix.src}
 		title="RemixIcon: Svelte-Remix"
 		href="https://www.npmjs.com/package/svelte-remix"
 	>
@@ -524,12 +524,12 @@
 			/>
 			<a href="https://svelte.dev/repl/60e7000b54004872aa4006535fcef7fd?version=3.48.0">REPL</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgSimple}
+		img={imgSimple.src}
 		title="Simple Icons: Svelte-Simples"
 		href="https://www.npmjs.com/package/svelte-simples"
 	>
@@ -549,12 +549,12 @@
 			>.<br />
 			<a href="https://github.com/shinokada/svelte-simples/blob/main/icon-names.md">Icon names</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgTabler}
+		img={imgTabler.src}
 		title="Tabler: Svelte-Tabler"
 		href="https://www.npmjs.com/package/svelte-tabler"
 	>
@@ -572,12 +572,12 @@
 			<a href="https://svelte.dev/repl/55ccaffe3d414235a6bb554ffd98f4bc?version=3.48.0">REPL 1</a>,
 			<a href="https://svelte.dev/repl/b63ed92a79a047bdb2017fac63624130?version=3.48.0">REPL 2</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgTeeny}
+		img={imgTeeny.src}
 		title="Teeny Icons: Svelte-Teenyicons"
 		href="https://www.npmjs.com/package/svelte-teenyicons"
 	>
@@ -596,12 +596,12 @@
 			600+ SVG icons from <a href="https://github.com/teenyicons/teenyicons">Teenyicons</a>.<br />
 			<a href="https://github.com/shinokada/svelte-teenyicons/blob/main/icon-list.md">Icon names</a>
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgTwemoji}
+		img={imgTwemoji.src}
 		title="Twitter Emoji: Svelte-Twitter-Emoji"
 		href="https://www.npmjs.com/package/svelte-twitter-emoji"
 	>
@@ -624,12 +624,12 @@
 				>Icon names</a
 			><br />
 		</p>
-	</EcommerceCard>
+	</Card>
 
-	<EcommerceCard
+	<Card
 		{divClass}
 		{headerClass}
-		img={imgWeather}
+		img={imgWeather.src}
 		title="Weather Icons: Svelte-Weather"
 		href="https://www.npmjs.com/package/svelte-weather"
 	>
@@ -651,5 +651,5 @@
 			<a href="https://github.com/shinokada/svelte-weather/blob/main/icon-list.md">Icon names</a><br
 			/>
 		</p>
-	</EcommerceCard>
+	</Card>
 </div>
