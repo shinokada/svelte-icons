6ddf8227c333f90295f7e3aab4a370b220c33dbd
Shinichi Okada <okada.shin@gmail.com>
2023-05-26 08:28:04 +0900

    fix: config files

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,11 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 3000
-	}
+		timeout: 240000,
+		port: 4173
+	},
+	testDir: 'tests',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
 
 export default config;
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+	preprocess: [vitePreprocess(), preprocess({
+		postcss: true
+	})],
 
 	kit: {
 		adapter: adapter()
